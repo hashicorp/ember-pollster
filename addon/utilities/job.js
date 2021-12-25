72a791885fa24a04b446f4b98f704462896a5f6b
Randall Morey <randall@randallmorey.com>
2021-12-25 17:27:16 -0500

    feature: implement Job primitive

---
+++ addon/utilities/job.js
@@ -0,0 +1,105 @@
+import { assert } from '@ember/debug';
+import { isTesting } from '@ember/debug/lib/testing';
+import { run } from '@ember/runloop';
+
+/**
+ * A job is a wrapper around a vanilla function `fn` which executes `fn` every
+ * `frequency` milliseconds.  Jobs may be started and stopped, as well as
+ * executed on a one-off basis.
+ * @module ember-pollster/utilities/job
+ */
+class Job {
+
+  // =properties
+
+  /**
+   * The wrapped function.
+   * @type {function}
+   */
+  fn;
+
+  /**
+   * Frequency in milliseconds to execute `fn`.
+   * @type {number}
+   */
+  frequency;
+
+  /**
+   * @private
+   * @type {boolean}
+   */
+  #running = false;
+
+  /**
+   * @private
+   * @type {?number}
+   */
+  #timeoutId;
+
+  /**
+   * @type {boolean}
+   */
+  get isTesting() {
+    return isTesting();
+  }
+
+  /**
+   * @readonly
+   * @type {boolean}
+   */
+  get running() {
+    return this.#running;
+  }
+
+  // =methods
+
+  constructor(fn, frequency) {
+    assert('`fn` is required.', fn);
+    assert('`frequency` in milliseconds is required.', frequency);
+    this.fn = fn;
+    this.frequency = frequency;
+  }
+
+  /**
+   * Starts the job and schedules the next run.  In Ember testing mode, runs
+   * are not scheduled and must be executed manually.
+   */
+  start() {
+    if (!this.#running) {
+      this.#running = true;
+      if (!this.isTesting) this.#queueNext();
+    }
+  }
+
+  /**
+   * Stops the job and cancels any pending run.
+   */
+  stop() {
+    this.#running = false;
+    clearTimeout(this.#timeoutId);
+  }
+
+  /**
+   * Executes `fn` within an Ember runloop.
+   * @return The result of calling `fn`.
+   */
+  run() {
+    return run(this.fn);
+  }
+
+  /**
+   * Schedules the next execution of `fn` (if in a running state) after
+   * `frequency` milliseconds;
+   * @private
+   */
+  #queueNext() {
+    this.#timeoutId = setTimeout(() => {
+      if (this.#running) {
+        this.run();
+        this.#queueNext();
+      }
+    }, this.frequency);
+  }
+}
+
+export default Job;
---
+++ tests/unit/utilities/job-test.js
@@ -0,0 +1,52 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import Job from 'ember-pollster/utilities/job';
+
+module('Unit | Utilities | job', function (hooks) {
+  setupTest(hooks);
+
+  test('it exists', function (assert) {
+    assert.expect(1);
+    assert.ok(Job);
+  });
+
+  test('it requires `fn` and `frequency`', function (assert) {
+    assert.expect(2);
+    assert.throws(() => new Job());
+    assert.throws(() => new Job(() => {}));
+  });
+
+  test('it can enter a running state (but not automatically execute) in test mode', function (assert) {
+    assert.expect(5);
+    let x = false;
+    const job = new Job(() => x = true, 1000);
+    assert.false(x, 'job has not executed yet');
+    assert.true(job.isTesting, 'job is in a test mode');
+    assert.false(job.running, 'job is in a stopped state');
+    job.start();
+    assert.false(x, 'job has not executed yet');
+    assert.true(job.running, 'job is in a running state');
+  });
+
+  test('it can start and stop', function (assert) {
+    assert.expect(3);
+    let x = false;
+    const job = new Job(() => x = true, 1000);
+    assert.false(job.running, 'job is in a stopped state');
+    job.start();
+    assert.true(job.running, 'job is in a running state');
+    job.stop();
+    assert.false(job.running, 'job is in a stopped state again');
+  });
+
+  test('it can be executed on a one-off basis', function (assert) {
+    assert.expect(4);
+    let x = false;
+    const job = new Job(() => x = true, 1000);
+    assert.false(x, 'job has not executed yet');
+    assert.false(job.running, 'job is in a stopped state');
+    job.run();
+    assert.true(x, 'job executed');
+    assert.false(job.running, 'job is still in a stopped state');
+  });
+});
